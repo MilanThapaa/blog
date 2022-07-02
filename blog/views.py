3c98f534677cff8c2d250539f43394df717dec2a
Milan Thapa <milanthapa299@gmail.com>
2022-07-02 12:54:17 +0545

    created list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    post = Post.published_objects.all()
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'blog/list.html', context)
