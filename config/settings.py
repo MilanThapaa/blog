3c36541219a1312d0bcf4163b66cdaab68d1b05b
Milan Thapa <milanthapa299@gmail.com>
2022-07-03 11:38:59 +0545

    Added email configuration

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email configuration
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "milanthapa299@gmail.com"
+EMAIL_HOST_PASSWORD = "qpcpghghrcngvkde"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
