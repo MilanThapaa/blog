379e8bb0ba2f40559be658ec7e192d317c4d3251
Milan Thapa <milanthapa299@gmail.com>
2022-07-04 10:41:24 +0545

    resolve similar post bug

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ def post_list(request, tag_slug=None):
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in=[tag])
-    paginator = Paginator(object_list, 1)
+    paginator = Paginator(object_list, 2)
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -56,8 +56,10 @@ def post_detail(request, year, month, day, post):
 
     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published_objects.filter(tags__in=post_tags_ids.exclude(id=post.id))
+    similar_posts = Post.published_objects.filter(tags__in=post_tags_ids).exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    # List of similar posts
 
     context = {
         'post': post,
