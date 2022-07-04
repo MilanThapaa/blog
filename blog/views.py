ddb7c7f6bc926792833190d6c60b84939c9c44cf
Milan Thapa <milanthapa299@gmail.com>
2022-07-04 09:28:34 +0545

    similar post completion

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -53,11 +54,17 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published_objects.filter(tags__in=post_tags_ids.exclude(id=post.id))
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     context = {
         'post': post,
         'comments': comments,
         'new_comment': new_comment,
-        'comment_form': comment_form
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
 
     }
 
